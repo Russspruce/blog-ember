e1d1381310a8301a9feef35d8de1da0cb7925dab
Andrew Russo and Catherine Karra <andrew@>
2016-06-09 09:21:00 -0700

    Completed save, delete, and update of blog posts.

--- app/components/blog-tile.js
+++ app/components/blog-tile.js
@@ -1,10 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateBlogPost: false,
   actions: {
+    update(blog, params) {
+      this.sendAction('update', blog, params);
+    },
     delete(blog) {
       if (confirm('Are you sure you want to delete this?')) {
-        this.sendAction('destroyBlog', rental);
+        this.sendAction('destroyBlog', blog);
       }
     }
   }
--- app/components/update-blog.js
+++ app/components/update-blog.js
@@ -1,20 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  addNewBlog: false,
+  updateBlogPost: false,
   actions: {
-    blogPostShow() {
-      this.set('addNewBlog', true);
+    updateBlogPost() {
+      this.set('updateBlogPost', true);
     },
-
-    sav1e() {
+    update(blog) {
       var params = {
         title: this.get('title'),
         date: this.get('date'),
         body: this.get('body'),
       };
-      this.set('addNewBlog', false);
-      this.sendAction('sav2e', params);
+      this.set('updateBlogPost', false);
+      this.sendAction('update', blog, params);
     }
   }
 });
